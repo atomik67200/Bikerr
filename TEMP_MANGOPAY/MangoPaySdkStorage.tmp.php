c9d0c9ec46a6fecd552e23fc107abfad10311304
atomik67200 <atomic67200@gmail.com>
2018-12-01 11:44:36 +0100

    correction bug

--- TEMP_MANGOPAY/MangoPaySdkStorage.tmp.php
+++
@@ -1,1 +0,0 @@
-<?php exit(); ?>O:29:"MangoPay\Libraries\OAuthToken":5:{s:11:"create_time";i:1532594299;s:12:"access_token";s:32:"86235d9d8ddc442187f5894ac47e8485";s:10:"token_type";s:6:"bearer";s:10:"expires_in";i:1199;s:17:"autentication_key";s:32:"eef08411730ca46933ac904cf5785519";}
